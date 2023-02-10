615ea9acbcf1268f5c6a3af7c52906a72acb368a
GidJoe <winterm@bfwob.de>
2023-02-10 12:53:37 +0100

    Printwriter Hinzugefügt

--- src/NQueenProblem.java
+++ src/NQueenProblem.java
@@ -1,7 +1,13 @@
+import java.io.*;
+
+import static java.lang.System.out;
+
 public class NQueenProblem {
     public static int damenAnzahl = 8;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
         int[][] schachbrett = {
                 {0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0},
@@ -14,7 +20,7 @@ public class NQueenProblem {
     };
 
         if (solve(schachbrett, 0) == false) {
-            System.out.print("Es existiert keine Lösung");
+            out.print("Es existiert keine Lösung");
             return;
         }
 
@@ -50,8 +56,11 @@ public class NQueenProblem {
     }
 
 
-    static boolean solve(int[][] schachbrett, int col) {
+    static boolean solve(int[][] schachbrett, int col) throws IOException {
         // Wenn alle Damen (von damenAnzahl) platziert sind, dann return TRUE
+
+
+
         if (col >= damenAnzahl)
             return true;
 
@@ -66,13 +75,35 @@ public class NQueenProblem {
                 schachbrett[i][col] = 1;
 
                 // Rekursiver Aufruf um den Rest der Damen zu platzieren
-                if (solve(schachbrett, col + 1))
-                    return true;
+                if (solve(schachbrett, col + 1)) {
+
+                    //PrintWriter -- ANFANG
+                    PrintWriter out = new PrintWriter(new FileOutputStream(
+                            new File("output.txt"),
+                            true /* append = true */));
+
+                    String stringForFileOutput = "";
+                    for (int x = 0; x < schachbrett.length; x++) {
+                        for (int j = 0; j < schachbrett.length; j++) {
+                            stringForFileOutput+= " " + schachbrett[x][j] + " ";
+                        }
+                        stringForFileOutput+= "\n";
+                    }
+                    out.append(stringForFileOutput.replace('0', '_').replace('1', 'D'));
+                    out.append("\n");
+                    out.append("\n");
+                    out.close();
+                    //PrintWriter -- ENDE
+
+                    return true;}
 
                 // Wenn die Platzierung auf schachbrett[i][col] nicht zu einem Ergebniss kommt, dann entferne die Dame wieder von
                 // schachbrett[i][col]
                 schachbrett[i][col] = 0; // BACKTRACK
+
             }
+
+
         }
 
         // Wenn die Dame nirgends in Row platziert werden kann, return false!
@@ -80,7 +111,7 @@ public class NQueenProblem {
     }
 
 
-    static void ausgabe(int[][] schachbrett) {
+    static void ausgabe(int[][] schachbrett) throws IOException {
         String Ausgabe = "";
         for (int i = 0; i < schachbrett.length; i++) {
             for (int j = 0; j < schachbrett.length; j++) {
@@ -88,18 +119,8 @@ public class NQueenProblem {
             }
             Ausgabe+= "\n";
         }
-        System.out.print(Ausgabe.replace('0', '_').replace('1', 'D'));
-    }
-
-
-//                static void ausgabe(int[][] schachbrett) {
-//        for (int i = 0; i < damenAnzahl; i++) {
-//            for (int j = 0; j < damenAnzahl; j++) {
-//                System.out.print(" " + schachbrett[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
-
+        System.out.println(Ausgabe.replace('0', '_').replace('1', 'D'));
 
     }
+
+ }
